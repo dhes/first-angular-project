39d4359969d7af34b07a3fffb4045d123ffbafb6
Dan Heslinga <drdan@heslingamd.com>
2019-08-08 18:31:49 -1000

    first commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    condition: "", 
+    name: "Prediabetes - annual A1c", 
+    description: "Patients with prediabetes should have an A1c blood test once a year.", 
+    implemented: true, 
+    gender: null,  
+    low_age: null, 
+    high_age: null, 
+    interval: 12, 
+    category: "management", 
+    tobacco_status: null, 
+    pack_years: null, 
+    years_since_quit_smoking: null, 
+    low_dob: null, 
+    high_dob: null
+  }
+]
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
