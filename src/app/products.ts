94d8db0d74dd6ea9a3a9c771339a03fc4d291f33
Dan Heslinga <drdan@heslingamd.com>
2019-08-08 18:58:29 -1000

    small correction in rule #2

--- src/app/products.ts
+++ src/app/products.ts
@@ -14,10 +14,10 @@ export const products = [
     years_since_quit_smoking: null, 
     low_dob: null, 
     high_dob: null
-  }
+  }, 
   {
     condition: "abdominal aortic aneurysm", 
-    lname: "Current smoker - AAA screening", 
+    name: "Current smoker - AAA screening", 
     description: "Screen men aged 65-75 y, who have ever smoked, 1 time for abdominal aortic aneurysm (AAA), using ultrasonography. ", 
     implemented: true, 
     gender: "M", 
